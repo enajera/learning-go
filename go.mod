d8f4e538072565859a338be0b9e063d4c64f81dd
Elvin Nájera <elvin7n@gmail.com>
2022-12-20 11:28:16 -0600

    commit

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/enajera/learning-go
+
+go 1.19
+
+require (
+	rsc.io/quote v1.5.2
+	rsc.io/quote/v3 v3.1.0
+)
+
+require (
+	golang.org/x/text v0.5.0 // indirect
+	rsc.io/sampler v1.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"github.com/enajera/learning-go/greet"
+	"rsc.io/quote"
+	quoteV3 "rsc.io/quote/v3"
 )
 
 func main() {
-	fmt.Println("Hello")
+	fmt.Println(greet.Italian())
+	fmt.Println(quote.Hello())
+	fmt.Println(quoteV3.Concurrency())
+	
 }
